6bdf38b5d2ef4579e8ed036fd18056ac56896c4f
Rafael <braga3.coelho3@gmail.com>
2025-07-14 19:41:06 -0300

    Migrar o banco de dados

---
+++ database/migrations/2025_07_14_222506_atribuir_permissoes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('espaco_user', function (Blueprint $table) {
+            $table->foreignId("espaco_id")
+                ->constrained("espacos")
+                ->onDelete("cascade");
+            $table->foreignId("user_id")
+                ->constrained("user")
+                ->onDelete("cascade");
+            $table->primary(['espaco_id', 'user_id']); // composite key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('espaco_permissao');
+    }
+};
