9d36f1dcc783d4015f7f5017182f3f9c3a2e47cc
andref03 <andref.lopes@outlook.com>
2025-07-21 19:33:35 -0300

    centrlizando logo icon, na página de login

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -6,7 +6,7 @@ interface AppLogoIconProps {
 
 export default function AppLogoIcon({ className }: AppLogoIconProps) {
     return (
-        <div className={cn("flex items-center", className)}>
+        <div className={cn("flex justify-center items-center", className)}>
             <img 
                 src="/logo_coruja_texto.png"
                 alt="Logo SAGE claro"
@@ -19,10 +19,6 @@ export default function AppLogoIcon({ className }: AppLogoIconProps) {
                 className="w-80 h-80 object-contain mb-30 hidden dark:block"
                 style={{ maxWidth: 'none', maxHeight: 'none' }}
             />
-
-            <div className="ml-2 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">SAGE</span>
-            </div>
         </div>
     );
 }
