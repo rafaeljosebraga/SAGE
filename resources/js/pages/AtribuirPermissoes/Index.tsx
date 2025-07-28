a3a0ace30a304623ae7dd620abb7126314580521
andref03 <andref.lopes@outlook.com>
2025-07-28 20:27:26 -0300

    corrigindo formatação do botão "+" de atribuir uma permissão

--- resources/js/pages/AtribuirPermissoes/Index.tsx
+++ resources/js/pages/AtribuirPermissoes/Index.tsx
@@ -46,12 +46,19 @@ export default function AtribuirPermissoesIndex({ users, espacos }: AtribuirPerm
                         variant="outline"
                         size="sm"
                         asChild
-                        className="bg-sidebar dark:bg-white hover:bg-[#EF7D4C] dark:hover:bg-[#EF7D4C] text-black dark:text-black"
-                    >
+                        className="
+                            bg-white dark:bg-black
+                            text-[#EF7D4C] dark:text-[#EF7D4C]
+                            border border-[#EF7D4C]
+                            hover:bg-[#EF7D4C] hover:text-white
+                            dark:hover:bg-[#EF7D4C] dark:hover:text-white
+                            transition-colors
+                        "
+                        >
                         <Link href={`atribuir-permissoes/${user.id}/criar`}>
-                            <Plus className="h-4 w-4 text-[#F26326]" />
+                            <Plus className="h-4 w-4" />
                         </Link>
-                    </Button>
+                        </Button>
                 </div>
             )
         }
